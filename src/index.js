075b5341770ec48a21ff6de819922f6638417268
nana562 <36802637+nana562@users.noreply.github.com>
2020-11-09 12:59:30 +0000

    initial todoMatic commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+const DATA = [
+  {id: "todo-0", name: "Eat", completed:"false"},
+  {id: "todo-1", name: "Sleep", completed:"true"},
+  {id: "todo-2", name: "Repeat", completed:"false"}
+];
+
+ReactDOM.render(
+    <App tasks={DATA}/>,
+  document.getElementById('root')
+);
