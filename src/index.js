81c90d06578e2e3fe65d212ed4feb3b499ef9882
nana562 <36802637+nana562@users.noreply.github.com>
2020-11-12 17:40:07 +0000

    Checkbox feature debugged and Filter button feature implemented

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-const DATA = [
-  {id: "todo-0", name: "Eat", completed:"false"},
-  {id: "todo-1", name: "Sleep", completed:"true"},
-  {id: "todo-2", name: "Repeat", completed:"false"}
-];
+const DATA = [{ id: "todo-0", name: "Eat", completed: "false" }];
 
-ReactDOM.render(
-    <App tasks={DATA}/>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App tasks={DATA} />, document.getElementById("root"));
